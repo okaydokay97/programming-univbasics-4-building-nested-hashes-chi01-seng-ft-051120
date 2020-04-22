be09461df7f85c31cdc20a4c43ec5eeff9e339a4
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-22 01:26:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  travel_methods = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names=> {}}
+  travel_methods = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}
   }
 
 end
@@ -15,10 +15,21 @@ def monopoly_with_third_tier
   travel_methods = {:railroads =>{
     :pieces => 4, 
     :rent_in_dollars => {
-      
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
     }, 
-    :names=> {}}
+    :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
+      }
+    }
   }
+  
+  
 end
 
 def monopoly_with_fourth_tier
