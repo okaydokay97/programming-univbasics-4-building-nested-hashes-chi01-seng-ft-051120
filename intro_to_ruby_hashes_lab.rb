0f3283b4e89dfdb550ed3e5cbb04a50d903d45fe
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-22 01:30:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,24 +10,24 @@ def monopoly_with_second_tier
 
 end
 
-# def monopoly_with_third_tier
-# def monopoly_with_second_tier
-#   travel_methods = {:railroads =>{
-#     :pieces => 4, 
-#     :rent_in_dollars => {
-#       :one_piece_owned => 25,
-#       :two_pieces_owned => 50,
-#       :three_pieces_owned => 100,
-#       :four_pieces_owned => 200
-#     }, 
-#     :names => {
-#       :reading_railroad => {},
-#       :pennsylvania_railroad => {},
-#       :b_and_o_railroad => {},
-#       :shortline_railroad => {}
-#       }
-#     }
-#   }
+def monopoly_with_third_tier
+def monopoly_with_second_tier
+  travel_methods = {:railroads =>{
+    :pieces => 4, 
+    :rent_in_dollars => {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
+    }, 
+    :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
+      }
+    }
+  }
   
   
 # end
